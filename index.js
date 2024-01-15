c565fbad4e290dd14bcc57ad050e424f3fd84c0e
mikeb-molina <142452320+mikeb-molina@users.noreply.github.com>
2024-01-15 10:12:16 -0500

    Updated index.js with connection uri

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ const { check, validationResult} = require('express-validator');
 
 const Movies= Models.Movie;
 const Users = Models.User;
-mongoose.connect('mongodb://localhost:27017/test', {
+//mongoose.connect('mongodb://localhost:27017/test', {
+    //useNewUrlParser: true, 
+    //useUnifiedTopology: true
+//});
+mongoose.connect( process.env.CONNECTION_URI, {
     useNewUrlParser: true, 
     useUnifiedTopology: true
 });
-
 
 
 const app = express();
