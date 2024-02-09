c65acd2fc03c885f5cdd89dcb810b917098177f9
mikeb-molina <142452320+mikeb-molina@users.noreply.github.com>
2024-02-09 09:18:31 -0500

    update cors

--- index.js
+++ index.js
@@ -27,16 +27,16 @@ app.use(express.urlencoded({extended: true}));
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'),{flags:'a'});
 const cors = require('cors');
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
-app.use(cors({
-    origin: (origin, callback) => {
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){ //if specific origin isnt found on list of allowed origins
-        let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
-        return callback(new Error(message), false);
-    }
-    return callback(null, true);
-    }
-}));
+app.use(cors());
+    //origin: (origin, callback) => {
+       // if(!origin) return callback(null, true);
+       // if(allowedOrigins.indexOf(origin) === -1){ //if specific origin isnt found on list of allowed origins
+        //let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
+        //return callback(new Error(message), false);
+    //}
+   // return callback(null, true);
+   // }
+//}));
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
